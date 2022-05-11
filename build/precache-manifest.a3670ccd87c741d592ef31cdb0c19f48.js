930c7e54c87af6350a1414f3fa8e17588cf81a33
Akshay Kumar U <akshayapsangi123@gmail.com>
2022-05-11 16:02:17 +0530

    all commit

---
+++ build/precache-manifest.a3670ccd87c741d592ef31cdb0c19f48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc02f19c0801331d87b8",
+    "url": "/static/css/main.776c1996.chunk.css"
+  },
+  {
+    "revision": "cc02f19c0801331d87b8",
+    "url": "/static/js/main.cc02f19c.chunk.js"
+  },
+  {
+    "revision": "3507accbdbc8afa51b9d",
+    "url": "/static/js/1.3507accb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c97ee2a3ed91e9de148f8d40b2d324e",
+    "url": "/static/media/twitter-logo.5c97ee2a.svg"
+  },
+  {
+    "revision": "fe478a4ba13e42c9462c2a1745ac51f3",
+    "url": "/index.html"
+  }
+];
